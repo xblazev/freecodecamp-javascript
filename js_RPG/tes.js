d552cf334c5cff08f3c31265d1037903b1b52d19
xblazev <raflirmt2@gmail.com>
2024-02-04 23:08:29 +0700

    done

---
+++ js_RPG/tes.js
@@ -0,0 +1,15 @@
+const locations = [
+  {
+    text: "halo",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+  },
+  {
+    "button text": [
+      "Buy 10 health (10 gold)",
+      "Buy weapon (30 gold)",
+      "Go to town square",
+    ],
+  },
+];
+console.log(locations[0]["text"]);
+let button;
